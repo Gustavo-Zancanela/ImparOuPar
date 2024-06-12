478000d8bde1b0bb30912030b77234aa1ecf5983
ALUNO <ALUNO@LAB04-PC32>
2024-06-12 20:17:04 -0300

    Commit project ImparOuPar

---
+++ src/ChkOddEven.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class ChkOddEven {
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Escreva um número entre 1 e 10:");
+        num = in.nextInt();
+        
+        if (num == 1)
+        {
+            System.out.println(num + " é ímpar");
+        }
+        
+        if (num == 2)    
+        {
+            System.out.println(num + " é par");
+        }
+        
+        
+         if (num == 3)
+        {
+            System.out.println(num + " é ímpar");
+        }
+         
+         
+         if (num == 4)    
+        {
+            System.out.println(num + " é par");
+        }
+         
+         
+          if (num == 5)
+        {
+            System.out.println(num + " é ímpar");
+        }
+          
+          
+          if (num == 6)    
+        {
+            System.out.println(num + " é par");
+        }
+          
+          
+           if (num == 7)
+        {
+            System.out.println(num + " é ímpar");
+        }
+           
+           
+           if (num == 8)    
+        {
+            System.out.println(num + " é par");
+        }
+           
+           
+            if (num == 9)
+        {
+            System.out.println(num + " é ímpar");
+        }
+            
+            
+            if (num == 10)    
+        {
+            System.out.println(num + " é par");
+        }
+    }
+}
